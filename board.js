e23c1271959fbe8aad0f622b547806d44c2937a5
tertius Jubber <tjubber@gmail.com>
2024-01-08 08:20:54 +0200

    added boundaries to gameboard

--- board.js
+++ board.js
@@ -8,4 +8,23 @@ class Board {
   getEmtyBoard() {
     return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
   }
+
+  //setting the play area
+  valid(p) {
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx;
+        let y = p.y + dy;
+        return this.insideWalls(x) && this.aboveFloor(y);
+      });
+    });
+  }
+
+  insideWalls(x) {
+    return x >= 0 && x < COLS;
+  }
+
+  aboveFloor(y) {
+    return y <= ROWS;
+  }
 }
